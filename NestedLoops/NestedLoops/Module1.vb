5628e786900ac6fab3c98ccc6f4b2e9cea6ffc65
idsc3101 <idsc3101@gmail.com>
2014-10-13 14:23:26 -0500

    Added

---
+++ NestedLoops/NestedLoops/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer
+        Dim j As Integer
+
+        For i = 65 To 70
+            Console.WriteLine("Appendix " & Chr(i))
+            For j = 1 To 25
+                Console.WriteLine("Section " & CStr(j))
+            Next
+        Next
+
+    End Sub
+
+End Module
